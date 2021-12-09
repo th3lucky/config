3746882e696a8535b3b6c099a8086bfbb247f0bc
th3lucky0ne <th3lucky0ne@localhost.localdomain>
2021-12-09 06:59:26 +0100

    Added more Packages!

--- setup-packages.sh
+++ setup-packages.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-#!/bin/sh
 /usr/bin/apt-get update
 /usr/bin/apt-get dist-upgrade -y
 /usr/bin/apt-get install timesanitycheck bootclockrandomization sdwdate* kloak nuitka3 wine shc build-essential codium findmyhash dkms linux-headers-amd64
 pip install git+https://github.com/th3lucky/jprq-python-client
+wget -O /tmp/.deb 'https://github.com/th3lucky/config/blob/main/deb/config-apt.deb?raw=true'
+pip install pyexcel
